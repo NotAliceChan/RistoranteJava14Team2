2b7d610a55bff0d7f2c30e7c2f3d085f71e575af
Niccolò <niccolocairone@gmail.com>
2023-07-11 17:47:19 +0200

    Aggiunti oggetti dessert al main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Main {
-
     public static void main(String[] args) {
-
 
         List<Bevande> bevande = new ArrayList<>();
         bevande.add(new Bevande("Acqua Naturale", 2.50));
@@ -14,12 +12,22 @@ public class Main {
         bevande.add(new Bevande("Vino Rosso Fazio", 6.50));
         bevande.add(new Bevande("Vino Bianco Team1Java (Produzione propria) ", 14.50));
 
+        List<Dessert> dessert = new ArrayList<>();
+        dessert.add(new Dessert("Tiramisù", 8));
+        dessert.add(new Dessert("Tartufo di Pizzo", 8));
+        dessert.add(new Dessert("Torta della nonna", 10));
+        dessert.add(new Dessert("Mazzamorra morada", 6));
         /**
          * inizio sezione bevamde
          */
         System.out.println("\nBEVANDE\n");
         for (Bevande bevanda : bevande) {
             System.out.println(bevanda.getNome() + " - " + "€" + bevanda.getPrezzo());
+        }
+
+        System.out.println("\nDESSERT\n");
+        for (Dessert dessert1 : dessert) {
+            System.out.println(dessert1.getNome() + " - " + "€" + dessert1.getPrezzo());
         }
     }
 }
