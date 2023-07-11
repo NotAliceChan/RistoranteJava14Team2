28684c4f895c4a4370297cc02218a720213c82aa
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-07-11 17:03:43 +0200

    Aggiunta classe bevande

--- src/Bevande.java
+++ src/Bevande.java
@@ -1,2 +1,33 @@
 public class Bevande {
+    private String nome;
+    private double prezzo;
+
+
+    public Bevande(String nome, double prezzo) {
+        this.nome = nome;
+        this.prezzo = prezzo;
+    }
+
+    //recupare il dato
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void printDetails() {
+        System.out.println(
+                this.nome + " - " + this.prezzo + " €"
+        );
+    }
 }
