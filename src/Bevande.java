91f77b39d6a036957781c67dc34db23e034f4ca9
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-07-11 17:05:57 +0200

    Aggiunto javadoc

--- src/Bevande.java
+++ src/Bevande.java
@@ -1,30 +1,61 @@
+/**
+ * The type Bevande.
+ */
 public class Bevande {
     private String nome;
     private double prezzo;
 
-
+    /**
+     * Instantiates a new Bevande.
+     *
+     * @param nome   the nome
+     * @param prezzo the prezzo
+     */
     public Bevande(String nome, double prezzo) {
         this.nome = nome;
         this.prezzo = prezzo;
     }
 
-    //recupare il dato
+    /**
+     * Gets nome.
+     *
+     * @return the nome
+     */
+//recupare il dato
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Sets nome.
+     *
+     * @param nome the nome
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Gets prezzo.
+     *
+     * @return the prezzo
+     */
     public double getPrezzo() {
         return prezzo;
     }
 
+    /**
+     * Sets prezzo.
+     *
+     * @param prezzo the prezzo
+     */
     public void setPrezzo(double prezzo) {
         this.prezzo = prezzo;
     }
 
+    /**
+     * Print details.
+     */
     public void printDetails() {
         System.out.println(
                 this.nome + " - " + this.prezzo + " €"
