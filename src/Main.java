912bfeb0ad42b22a20bc8cb47bcc8f135e17db48
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-07-11 17:34:19 +0200

    Aggiunti oggetti bevande

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
 
+    public static void main(String[] args) {
 
 
+        List<Bevande> bevande = new ArrayList<>();
+        bevande.add(new Bevande("Acqua Naturale", 2.50));
+        bevande.add(new Bevande("Coca cola", 3.00));
+        bevande.add(new Bevande("Birra Peroni", 2.00));
+        bevande.add(new Bevande("Fanta", 3.00));
+        bevande.add(new Bevande("Vino Rosso Fazio", 6.50));
+        bevande.add(new Bevande("Vino Bianco Team1Java (Produzione propria) ", 14.50));
 
+        /**
+         * inizio sezione bevamde
+         */
+        System.out.println("\nBEVANDE\n");
+        for (Bevande bevanda : bevande) {
+            System.out.println(bevanda.getNome() + " - " + "€" + bevanda.getPrezzo());
+        }
+    }
 }
