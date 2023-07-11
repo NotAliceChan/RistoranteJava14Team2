197cf9ec33b29516b3a526a8adef565603358762
Sall <salvatoreparisi97@gmail.com>
2023-07-11 16:38:10 +0200

    aggiunto classe SecondiPiatti.

--- src/PrimiPiatti.java
+++ src/PrimiPiatti.java
@@ -1,31 +1,61 @@
-package src;
-
+/**
+ * The type Primi piatti.
+ */
 public class PrimiPiatti {
 
     private String nome;
     private double prezzo;
 
+    /**
+     * Instantiates a new Primi piatti.
+     *
+     * @param nome   the nome
+     * @param prezzo the prezzo
+     */
     public PrimiPiatti(String nome, double prezzo) {
         this.nome = nome;
         this.prezzo = prezzo;
     }
 
+    /**
+     * Gets nome.
+     *
+     * @return the nome
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Sets nome.
+     *
+     * @param nome the nome
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Gets prezzo.
+     *
+     * @return the prezzo
+     */
     public double getPrezzo() {
         return prezzo;
     }
 
+    /**
+     * Sets prezzo.
+     *
+     * @param prezzo the prezzo
+     */
     public void setPrezzo(double prezzo) {
         this.prezzo = prezzo;
     }
 
+    /**
+     * Print details.
+     */
     public void printDetails() {
         System.out.println(
                 this.nome + " - " + prezzo + " €"
--- src/SecondiPiatti.java
+++ src/SecondiPiatti.java
@@ -1,4 +1,63 @@
-package src;
-
+/**
+ * The type Secondi piatti.
+ */
 public class SecondiPiatti {
+    private String nome;
+    private double prezzo;
+
+    /**
+     * Instantiates a new Secondi piatti.
+     *
+     * @param nome   the nome
+     * @param prezzo the prezzo
+     */
+    public SecondiPiatti(String nome, double prezzo) {
+        this.nome = nome;
+        this.prezzo = prezzo;
+    }
+
+    /**
+     * Gets nome.
+     *
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * Sets nome.
+     *
+     * @param nome the nome
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * Gets prezzo.
+     *
+     * @return the prezzo
+     */
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    /**
+     * Sets prezzo.
+     *
+     * @param prezzo the prezzo
+     */
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    /**
+     * Print details.
+     */
+    public void printDetails() {
+        System.out.println(
+                this.nome + " - " + prezzo + " €"
+        );
+    }
 }
