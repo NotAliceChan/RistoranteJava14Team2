f3a2dc87f80fb76e50f5de931c803697162805ff
Alice <fenuwu99@gmail.com>
2023-07-13 11:17:47 +0200

    traformato i primi piatti del menu txt in codice con ciclo for, corretto txt i prezzi, modificati alcuni prezzi

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
+        List<PrimiPiatti> primiPiatti = new ArrayList<>();
+        primiPiatti.add(new PrimiPiatti("Carpaccio Maguro", 18.50));
+        primiPiatti.add(new PrimiPiatti("Tartarre di Salmone", 14.80));
+        primiPiatti.add(new PrimiPiatti("Burrikama", 22.90));
+
         List<SecondiPiatti> secondiPiatti = new ArrayList<>();
-        secondiPiatti.add(new SecondiPiatti("Insalata di Polpo e Patate", 18));
-        secondiPiatti.add(new SecondiPiatti("Salmone Grigliato", 25));
-        secondiPiatti.add(new SecondiPiatti("-Seppie con Piselli", 16));
+        secondiPiatti.add(new SecondiPiatti("Insalata di Polpo e Patate", 18.80));
+        secondiPiatti.add(new SecondiPiatti("Salmone Grigliato", 25.90));
+        secondiPiatti.add(new SecondiPiatti("-Seppie con Piselli", 16.40));
 
         List<Bevande> bevande = new ArrayList<>();
         bevande.add(new Bevande("Acqua Naturale", 2.50));
@@ -18,17 +23,20 @@ public class Main {
         bevande.add(new Bevande("Vino Bianco Team1Java (Produzione propria) ", 14.50));
 
         List<Dessert> dessert = new ArrayList<>();
-        dessert.add(new Dessert("Tiramisù", 8));
-        dessert.add(new Dessert("Tartufo di Pizzo", 8));
-        dessert.add(new Dessert("Torta della nonna", 10));
-        dessert.add(new Dessert("Mazzamorra morada", 6));
+        dessert.add(new Dessert("Tiramisù", 8.00));
+        dessert.add(new Dessert("Tartufo di Pizzo", 8.40));
+        dessert.add(new Dessert("Torta della nonna", 10.90));
+        dessert.add(new Dessert("Mazzamorra morada", 6.70));
         /**
          * inizio sezione bevamde
          */
 
         System.out.println("RISTORANTE LA SIRENETTA\n");
 
-
+        System.out.println("\nPrimi Piatti\n");
+        for (PrimiPiatti primopiatto : primiPiatti ){
+            System.out.println(primopiatto.getNome() + " - " + "€" + primopiatto.getPrezzo());
+        }
 
         System.out.println("\nSECONDI PIATTI\n");
         for (SecondiPiatti secondoPiatto : secondiPiatti) {
@@ -44,5 +52,12 @@ public class Main {
         for (Dessert dessert1 : dessert) {
             System.out.println(dessert1.getNome() + " - " + "€" + dessert1.getPrezzo());
         }
+
+
+
+
     }
+
+
+
 }
