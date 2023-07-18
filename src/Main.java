92f061ba316b5bfa0d9e9cfeecde2633566901ca
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-07-18 15:42:00 +0200

    Aggiunti oggetti bevande

--- src/Main.java
+++ src/Main.java
@@ -10,26 +10,24 @@ public class Main {
         primiPiatti.add(new PrimiPiatti("Tartarre di Salmone", 14.80));
         primiPiatti.add(new PrimiPiatti("Burrikama", 22.90));
 
-        menu.addPiattoBevandaDessert(new SecondiPiatti("Insalata di Polpo e Patate", 18.80));
-        menu.addPiattoBevandaDessert(new SecondiPiatti("Salmone Grigliato", 25.90));
-        menu.addPiattoBevandaDessert(new SecondiPiatti("Seppie con Piselli", 16.40));
+        menu.addSecondoPiatto(new SecondiPiatti("Insalata di Polpo e Patate", 18.80));
+        menu.addSecondoPiatto(new SecondiPiatti("Salmone Grigliato", 25.90));
+        menu.addSecondoPiatto(new SecondiPiatti("Seppie con Piselli", 16.40));
 
-        List<Bevande> bevande = new ArrayList<>();
-        bevande.add(new Bevande("Acqua Naturale", 2.50));
-        bevande.add(new Bevande("Coca cola", 3.00));
-        bevande.add(new Bevande("Birra Peroni", 2.00));
-        bevande.add(new Bevande("Fanta", 3.00));
-        bevande.add(new Bevande("Vino Rosso Fazio", 6.50));
-        bevande.add(new Bevande("Vino Bianco Team1Java (Produzione propria) ", 14.50));
+
+        menu.addBevanda(new Bevande("Acqua Naturale", 2.50));
+        menu.addBevanda(new Bevande("Coca cola", 3.00));
+        menu.addBevanda(new Bevande("Birra Peroni", 2.00));
+        menu.addBevanda(new Bevande("Fanta", 3.00));
+        menu.addBevanda(new Bevande("Vino Rosso Fazio", 6.50));
+        menu.addBevanda(new Bevande("Vino Bianco Team1Java (Produzione propria) ", 14.50));
 
         List<Dessert> dessert = new ArrayList<>();
         dessert.add(new Dessert("Tiramisù", 8.00));
         dessert.add(new Dessert("Tartufo di Pizzo", 8.40));
         dessert.add(new Dessert("Torta della nonna", 10.90));
         dessert.add(new Dessert("Mazzamorra morada", 6.70));
-        /**
-         * inizio sezione bevamde
-         */
+
 
         System.out.println("RISTORANTE LA SIRENETTA\n");
 
@@ -40,10 +38,7 @@ public class Main {
 
         menu.printMenu();
 
-        System.out.println("\nBEVANDE\n");
-        for (Bevande bevanda : bevande) {
-            System.out.println(bevanda.getNome() + " - " + "€" + bevanda.getPrezzo());
-        }
+
 
         System.out.println("\nDESSERT\n");
         for (Dessert dessert1 : dessert) {
--- src/Menu.java
+++ src/Menu.java
@@ -6,31 +6,42 @@ public class Menu {
 
 
     private List<SecondiPiatti> secondiPiatti;
+    private List<Bevande> bevande;
     private boolean stampaScrittaSecondiUnaVolta = false;
+    private Boolean stampaScrittaBevandeUnavolta = false;
 
     //TODO aggiungete all'interno del costruttore il vostro new ArrayList<>()
 
     public Menu() {
         this.secondiPiatti = new ArrayList<>();
+        this.bevande = new ArrayList<>();
     }
+
 
     //TODO create diversi metodi chiamati sempre addPiattoBevandaDessert come sotto. Ognuno di questi accetta un parametro  in base al tipo e aggiunge un oggetto di quel tipo come nell'esempio sotto.
 
-    public void addPiattoBevandaDessert(SecondiPiatti secondoPiatto){
+    public void addSecondoPiatto(SecondiPiatti secondoPiatto){
         this.secondiPiatti.add(secondoPiatto);
     }
+    public void addBevanda(Bevande bevanda) { this.bevande.add(bevanda);}
 
     //TODO create diversi metodi chiamati sempre setPiattoBevandaDessert come sotto. Ognuno di questi accetta un parametro in base al tipo e lo modifica come l'esempio sotto.
 
-    public void setPiattoBevandaDessert(int index, SecondiPiatti element) {
+    public void setPiattoSecondo(int index, SecondiPiatti element) {
         this.secondiPiatti.set(index, element);
+    }
+    public void setBevanda(int index, Bevande element) {
+        this.bevande.set(index, element);
     }
 
 
     //TODO create diversi metodi chiamati sempre removePiattoBevandaDessert come sotto. Ognuno di questi accetta un parametro index e rimuove dalla lista l'elemento in base all'indice come l'esempio sotto.
 
-    public void  removePiattoBevandaDessert(int index){
+    public void  removePiattoSecondo(int index){
         this.secondiPiatti.remove(index);
+    }
+    public void  removeBevanda(int index){
+        this.bevande.remove(index);
     }
 
     //TODO aggiungere un ciclo for a questo metodo che stampa il vostro tipo come nell'esempio qui sotto.
@@ -41,9 +52,17 @@ public class Menu {
                 System.out.println("\nSECONDI PIATTI\n");
                 stampaScrittaSecondiUnaVolta = true;
             }
+
             System.out.println(piattoS.getNome() + " - " + "€" + piattoS.getPrezzo());
         }
+        for (Bevande bevanda : this.bevande) {
+            if (!stampaScrittaBevandeUnavolta) {
+                System.out.println("\nBEVANDE\n");
+                stampaScrittaBevandeUnavolta = true;
+            }
 
+            System.out.println(bevanda.getNome() + " - " + "€" + bevanda.getPrezzo());
+        }
 
     }
 }
